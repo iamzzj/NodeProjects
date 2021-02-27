cd370a2a7358fcd88211be5215d1d85015ae0166
iamzzj <iamzzj@qq.com>
2021-02-27 22:18:58 +0800

    webpack-base add

---
+++ webpack-base/src/components/App.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <h1>
+            this is APP root
+        </h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+
+            }
+        },
+        methods: {
+
+        }
+    }
+</script>
+
+<style scoped>
+    h1 {
+        color: red;
+    }
+</style>
